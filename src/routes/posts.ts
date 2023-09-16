437f1d4e444aa8c422f3f8ee7a1a83aca48cef92
onishi taisei <taisei.developer@gmail.com>
2023-09-16 23:44:42 +0900

    add: 新規投稿用のroutesを追加

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -6,4 +6,6 @@ import postsController from "../controllers/posts-controller";
 // 全ての投稿を取得
 router.get("/", postsController.fetchAllPosts);
 
+// 新規投稿を作成する
+router.post("/", postsController.fetchAllPosts);
 module.exports = router;
