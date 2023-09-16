d9270ded9a62dcc3fc686487bef0776a68522872
onishi taisei <taisei.developer@gmail.com>
2023-09-16 23:44:18 +0900

    add: controllerを作成

--- src/controllers/posts-controller.ts
+++ src/controllers/posts-controller.ts
@@ -6,6 +6,50 @@ class PostController {
         const posts = await Post.fetchAllPosts();
         res.json(posts).status(200);
     }
+
+    async createPost(req: Request, res: Response) {
+        const { userId, title, body } = req.body;
+        let errors = [];
+
+        if (!userId) {
+            errors.push("userId is required");
+        }
+        if (!title) {
+            errors.push("title is required");
+        }
+        if (!body) {
+            errors.push("body is required");
+        }
+
+        if (errors.length > 0) {
+            // 400 Bad Request（クライアント側のリクエストに誤りがある）
+            return res.status(400).send({ error: errors.join(", ") });
+        }
+
+        const newPost = {
+            userId: userId,
+            title: title,
+            body: body,
+        };
+
+        // 201 Created（POSTリクエストが成功）
+        // 成功した投稿内容を返却
+        try {
+            await Post.createPost(userId, title, body);
+            res.status(201).send({ message: "Post created successfully!" });
+        } catch (error) {
+            if (isError(error)) {
+                res.status(500).send({ error: error.message });
+            }
+        }
+        res.status(201).send(newPost);
+    }
+}
+
+// カスタム型ガード
+// todo: 別APIでも使用するためutilに移動させる
+function isError(err: any): err is Error {
+    return err instanceof Error;
 }
 
 export default new PostController();
